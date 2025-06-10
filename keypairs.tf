81d04bbee75fd7d461cc440799c67db579fca6ce
roberto <rcastellong98@gmail.com>
2025-06-10 16:25:10 +0200

    key pair resource created

---
+++ keypairs.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "vprofilekey" {
+  key_name   = "vprofilekey"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaRO/Bb/cmGj+aX4VX8J8ld6CzWMMmtdMd/rkQHIodw DEll@DESKTOP-29FU49E"
+}
