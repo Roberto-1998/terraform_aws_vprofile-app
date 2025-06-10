3b2f0f4cf661e6a7b0f66c3a356f814b43f74e91
roberto <rcastellong98@gmail.com>
2025-06-10 16:12:35 +0200

    variables ready

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "AWS_REGION" {}
+
+variable "PRIV_KEY_PATH" {}
+
+variable "PUB_KEY_PATH" {}
+
+variable "USERNAME" {}
+
+variable "MYIP" {}
+
+variable "rmquser" {}
+
+variable "rmqpass" {}
+
+variable "dbuser" {}
+
+variable "dbpass" {}
+
+variable "dbname" {}
+
+variable "instance_count" {}
+
+variable "VPC_NAME" {}
+
+variable "Zone1" {}
+
+variable "Zone2" {}
+
+variable "Zone3" {}
+
+variable "VpcCIDR" {}
+
+variable "PubSub1CIDR" {}
+
+variable "PubSub2CIDR" {}
+
+variable "PubSub3CIDR" {}
+
+variable "PrivSub1CIDR" {}
+
+variable "PrivSub2CIDR" {}
+
+variable "PrivSub3CIDR" {}
+
+variable "PROJECT" {}
